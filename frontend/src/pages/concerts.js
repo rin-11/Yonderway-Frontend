9c8fa736f639aca987411d00dabc9e4d2aec20fd
Rin Kates <erinkates11@gmail.com>
2023-04-06 21:50:14 -0400

    MVCR setup and pages complete

---
+++ frontend/src/pages/concerts.js
@@ -0,0 +1,8 @@
+import { Link } from "react-router-dom";
+
+const Concerts = (props) => {
+    return <h1>This is the Concerts Component</h1>;
+}
+
+export default Concerts;
+
---
+++ frontend/src/pages/main.js
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+const Main = (props) => {
+    return <h1>This is the Main Component</h1>;
+}
+
+export default Main
+
+
