508f4b9320cd07e4bab80512ec2e2866892b86ca
Rin Kates <erinkates11@gmail.com>
2023-04-07 00:01:09 -0400

    backend set up and mongoose installed

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+// Import Dependencies
+const express = require("express");
+const cors = require("cors");
+
+// Import JSON files
+    // const filename = require('./nameofile.json')
+
+// Create our app object
+const app = express();
+
+// MIDDLEWARE
+app.use(cors());
+
+//home route for testing our app
+app.get("/", (req, res) => {
+  res.send("working");
+});
+
+// route for retrieving API data
+// app.get("/linkname", (req, res) => {
+//   res.json(APIdata);
+// });
+
+
+//declare a variable for our port number
+const PORT = process.env.PORT || 4000;
+
+// turn on the server listener
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
