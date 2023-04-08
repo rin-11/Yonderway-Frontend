712f0739eb2a3d9f50c70e09df6a204a57a1e94f
Ronsou <93101540+SouCode@users.noreply.github.com>
2023-04-08 13:03:22 +0000

    Updated Connection to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,12 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const port = 3000;
+const database = require('./utils/database');
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => {
-  console.log('MongoDB connected');
-})
-.catch((err) => {
-  console.log('MongoDB connection error:', err);
+database.connect(); // Call the connect() function in the database.js file to establish a connection to the MongoDB database
+
+// Define your routes and middleware here
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
