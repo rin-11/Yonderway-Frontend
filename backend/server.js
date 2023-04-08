a26a8217b37282cc47aad59fe4df6c8305829be4
Ronsou <93101540+SouCode@users.noreply.github.com>
2023-04-08 03:35:37 +0000

    Connected MongoDB to server

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('MongoDB connected');
+})
+.catch((err) => {
+  console.log('MongoDB connection error:', err);
+});
