4a0e9feceb794a5bb4b437a8b37ad700fd53e0c9
be-no <18bngo@gmail.com>
2023-04-10 01:11:12 -0700

    footer & search bar completed

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,9 +1,43 @@
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+// npm install google-maps-react
+// import { Map, GoogleApiWrapper } from 'google-maps-react';
+//Display Search by Image and Search Bar 
 
 const Main = (props) => {
-    return <h1>This is the Main Component</h1>;
+    const [searchQuery, setSearchQuery] = useState('');
+    const [destination, setDestination] = useState(null);
+
+    const handleInputChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
+    // const handleSearch = () => {
+    //     const { google } = props;
+    //     const service = new google.maps.places.PlacesService(document.createElement('div'));
+    //     service.textSearch({ query: searchQuery }, (results, status) => {
+    //         if (status === google.maps.places.PlacesServiceStatus.OK) {
+    //             const { geometry: { location } } = results[0];
+    //             setDestination(location);
+    //         } else {
+    //             console.error('Error searching for city:', status);
+    //         }
+    //     });
+    // };
+
+  return (
+    <div>
+        <h1>Search by City</h1>
+      <div>
+        <input type="text" value={searchQuery} onChange={handleInputChange} />
+        {/* <button onClick={handleSearch}>Search</button> */}
+      </div>
+      {/* <Map google={props.google} center={destination} /> */}
+    </div>
+  );
 }
 
 export default Main;
 
-
+// export default GoogleApiWrapper({
+//   apiKey: 'YOUR_API_KEY'
+// })(Main);
