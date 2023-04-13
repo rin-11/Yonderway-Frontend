6133a90031a8820c4b6a5e7fb857d187a4d5aa81
Ronsou <93101540+SouCode@users.noreply.github.com>
2023-04-13 00:22:41 +0000

    Full comments made

--- src/pages/main.js
+++ src/pages/main.js
@@ -33,4 +33,27 @@ const Main = (props) => {
 
   // Function to render destination images
   const renderDestinations = () => {
-    // Map through the destinations array and return an image element for each
+    // Map through the destinations array and return an image element for each destination
+    return destinations.map((destination, index) => (
+      <div key={index}>
+        <img src={destination} alt="Destination" />
+      </div>
+    ));
+  };
+
+  return (
+    <div>
+      <h1>Search by City</h1>
+      <div>
+        {/* Input field for entering the destination city */}
+        <input type="text" placeholder="Enter your destination" value={searchQuery} onChange={handleInputChange} />
+        {/* Link component from react-router-dom to navigate to the restaurants page with the searchQuery as a URL parameter */}
+        <Link to={`/restaurants?city=${searchQuery}`}><button>Search</button></Link>
+      </div>
+      {/* Render destination images only if the destinations array has data */}
+      {destinations.length > 0 ? renderDestinations() : null}
+    </div>
+  );
+};
+
+export default Main;
--- src/pages/restaurants.js
+++ src/pages/restaurants.js
@@ -3,32 +3,43 @@ import api from '../utils/api';
 import { useLocation } from 'react-router-dom';
 
 const Restaurants = (props) => {
+  // Declare state variable for storing restaurant data
   const [restaurants, setRestaurants] = useState([]);
+  // Get the location object from the react-router-dom library
   const location = useLocation();
 
+  // Fetch restaurants on component mount or when the location changes
   useEffect(() => {
+    // Extract the 'city' search parameter from the URL
     const searchParams = new URLSearchParams(location.search);
     const city = searchParams.get('city');
+    // If the 'city' parameter exists, fetch restaurants for the specified city
     if (city) {
       fetchRestaurants(city);
     }
   }, [location]);
 
+  // Function to fetch restaurants data from the API
   const fetchRestaurants = async (city) => {
     try {
+      // Send GET request to the /restaurant/:city endpoint
       const response = await api.get(`/restaurant/${city}`);
+      // Update the restaurants state with the received data
       setRestaurants(response.data.data);
     } catch (error) {
       console.error(error);
     }
   };
 
+  // Function to render restaurant data
   const renderRestaurants = () => {
+    // Map through the restaurants array and return a div element with restaurant details for each restaurant
     return restaurants.map((restaurant, index) => (
       <div key={index}>
         <h2>{restaurant.name}</h2>
         <p>Rating: {restaurant.rating}</p>
         <p>Description: {restaurant.description}</p>
+        {/* Check if a photo is available for the restaurant, and render it if it exists */}
         {restaurant.photo ? (
           <img src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${restaurant.photo}&key=${process.env.REACT_APP_GOOGLE_KEY}`} alt="Restaurant" />
         ) : null}
@@ -38,6 +49,7 @@ const Restaurants = (props) => {
 
   return (
     <div>
+      {/* Render restaurant data only if the restaurants array has data */}
       {restaurants.length > 0 ? renderRestaurants() : null}
     </div>
   );
