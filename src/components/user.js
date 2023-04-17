f31f6d07889d234b4287dee60726630bf04f7102
Rin Kates <erinkates11@gmail.com>
2023-04-17 13:23:30 -0400

    user register inputs working

--- src/components/user.js
+++ src/components/user.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
 //Account: Create account and sign in 
 //Once the user creates an account: add conditional to hide sigin/create acount buttons and replace with login /log out buttons 
 
 const Usernav = (props) => {
+  
   return (
     <nav className='usernav'>
         <Link to='/register'>
