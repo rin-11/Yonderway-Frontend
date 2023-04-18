38b531f3ccce0552f77336459affb3a53c1135f3
User Name <jessnsq.codes@gmail.com>
2023-04-17 22:46:56 -0400

    hearts added to restaurants and attractions

--- src/pages/City.js
+++ src/pages/City.js
@@ -69,11 +69,15 @@ const City = (props) => {
   const renderHotels = () => {
     return hotels.map((hotel, index) => (
       <div className='activity_container' key={index}>
-        <Link to={`/activity`}>
+        {/* <Link to={`/activity`}>  */}
+        <button className='add-wish'> 
+        <img src='https://whatemoji.org/wp-content/uploads/2020/07/Red-Heart-Emoji.png' id='star1'></img> 
+        </button>
+
           {hotel.photo ? (
             <img src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${hotel.photo}&key=${process.env.REACT_APP_GOOGLE_KEY}`} alt="Hotel" className='activities' />
           ) : null}
-        </Link>
+        {/* </Link> */}
         <div>
           <h2 className='act-name'>{hotel.name}</h2>
           <div className='rating'>
@@ -90,6 +94,11 @@ const City = (props) => {
   const renderRestaurants = () => {
     return restaurants.map((restaurant, index) => (
       <div className='activity_container' key={index}>
+
+        <button className='add-wish'> 
+        <img src='https://whatemoji.org/wp-content/uploads/2020/07/Red-Heart-Emoji.png' id='star1'></img> 
+        </button>
+
         {restaurant.photo ? (
           <img src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${restaurant.photo}&key=${process.env.REACT_APP_GOOGLE_KEY}`} alt="Restaurant" className='activities' />
         ) : null}
@@ -108,6 +117,11 @@ const City = (props) => {
   const renderAttractions = () => {
     return attractions.map((attraction, index) => (
       <div className='activity_container' key={index}>
+
+        <button className='add-wish'> 
+        <img src='https://whatemoji.org/wp-content/uploads/2020/07/Red-Heart-Emoji.png' id='star1'></img> 
+        </button>
+
         {attraction.photo ? (
           <img src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${attraction.photo}&key=${process.env.REACT_APP_GOOGLE_KEY}`} alt="Attraction" className='activities' />
         ) : null}
