91383bb95b89699fa8019f2c79648e49565fc2cb
Sou <samelsonjohn@protonmail.com>
2023-04-20 22:31:22 -0400

    limited the API Results

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
-// Create an instance of axios with a custom configuration
 const api = axios.create({
-  // Set the base URL for all API requests using the environment variable REACT_APP_API_URL
-  baseURL: `${process.env.REACT_APP_API_URL}`,
+  baseURL: process.env.REACT_APP_API_URL, // Use the REACT_APP_API_URL variable from your .env file
 });
 
-// Export the customized axios instance for us ein other parts of of the application
 export default api;
-
